5025a543714c3970e0fc7806077737728fa3f7cd
nguyen.trung <nguyen.trung@sharing-innovations.com>
2021-12-25 13:08:23 +0700

    form validation message th:errorclass, th:errors and #fields.hasError()

--- src/main/java/com/example/demo/controller/SignUpController.java
+++ src/main/java/com/example/demo/controller/SignUpController.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,7 +37,14 @@ public class SignUpController {
      * User signup process
      */
     @PostMapping("/singup")
-    public String postSignup(@ModelAttribute SignupForm form) {
+    public String postSignup(Model model, Locale locale,
+                             @ModelAttribute SignupForm form,
+                             BindingResult bindingResult) {
+        //input check result
+        if (bindingResult.hasErrors()){
+            //NG: return the user signup screen
+            return getSignup(model, locale, form);
+        }
         log.info(form.toString());
         //model.addAttribute("signupForm", form)
         return "redirect:/login"; //PRG
