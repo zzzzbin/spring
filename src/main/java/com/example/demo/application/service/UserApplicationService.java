0f4c13d6e23da73780623242aeb4e66effa5bfeb
nguyen.trung <nguyen.trung@sharing-innovations.com>
2021-12-25 09:49:26 +0700

    use MessageSource to get message.properties from service

--- src/main/java/com/example/demo/application/service/UserApplicationService.java
+++ src/main/java/com/example/demo/application/service/UserApplicationService.java
@@ -1,16 +1,24 @@
 package com.example.demo.application.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 
 import java.util.LinkedHashMap;
+import java.util.Locale;
 import java.util.Map;
 
 @Service
 public class UserApplicationService {
+    @Autowired
+    private MessageSource messageSource;
+
     public Map<String, Integer> getGenderMap(){
         Map<String, Integer> genderMap = new LinkedHashMap<>();
-        genderMap.put("male", 1);
-        genderMap.put("female", 2);
+        String male = messageSource.getMessage("male",null, Locale.ENGLISH);
+        String female = messageSource.getMessage("female", null, Locale.ENGLISH);
+        genderMap.put(male, 1);
+        genderMap.put(female, 2);
         return genderMap;
     }
 }
