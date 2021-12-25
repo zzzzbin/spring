1e462346e4fe7c5353e54c5d1f85868e511550f4
nguyen.trung <nguyen.trung@sharing-innovations.com>
2021-12-25 13:25:47 +0700

    use spring valiation lib

--- src/main/java/com/example/demo/controller/SignUpController.java
+++ src/main/java/com/example/demo/controller/SignUpController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,7 +39,7 @@ public class SignUpController {
      */
     @PostMapping("/singup")
     public String postSignup(Model model, Locale locale,
-                             @ModelAttribute SignupForm form,
+                             @ModelAttribute @Validated SignupForm form,
                              BindingResult bindingResult) {
         //input check result
         if (bindingResult.hasErrors()){
--- src/main/java/com/example/demo/form/SignupForm.java
+++ src/main/java/com/example/demo/form/SignupForm.java
@@ -1,17 +1,34 @@
 package com.example.demo.form;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.*;
 import java.util.Date;
 
 @Data
 public class SignupForm {
+    @NotBlank
+    @Email
     private String userId;
+
+    @NotBlank
+    @Length(min = 4, max = 100)
+    @Pattern(regexp = "^[a-zA-Z0-9]+$")
     private String password;
+
+    @NotBlank
     private String userName;
+
     @DateTimeFormat(pattern = "dd/MM/yyyy")
+    @NotNull
     private Date birthday;
+
+    @Min(20)
+    @Max(100)
     private Integer age;
+
+    @NotNull
     private Integer gender;
 }
