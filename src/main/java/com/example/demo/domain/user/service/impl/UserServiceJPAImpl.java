c0e15f05021372a6d6da1a95fd3b4b7751327999
nguyen.trung <nguyen.trung@sharing-innovations.com>
2022-01-02 13:33:17 +0700

    switch to sping data jpa

---
+++ src/main/java/com/example/demo/domain/user/service/impl/UserServiceJPAImpl.java
@@ -0,0 +1,71 @@
+package com.example.demo.domain.user.service.impl;
+
+import com.example.demo.domain.user.model.MUser;
+import com.example.demo.domain.user.service.UserService;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.dao.DataAccessException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Primary
+public class UserServiceJPAImpl implements UserService {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Transactional
+    @Override
+    public void signup(MUser user) {
+        //existence check
+        boolean exists = userRepository.existsById(user.getUserId());
+        if (exists) {
+            throw new DataAccessException("User already exists") {
+            };
+        }
+
+        user.setDepartmentId(1);
+        user.setRole("ROLE_GENERAL");
+
+        String rawPassword = user.getPassword();
+        user.setPassword(passwordEncoder.encode(rawPassword));
+        userRepository.save(user);
+    }
+
+    @Override
+    public List<MUser> getUsers(MUser user) {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public MUser getUserByUserId(String userId) {
+        Optional<MUser> option = userRepository.findById(userId);
+        MUser mUser = option.orElse(null);
+        return mUser;
+    }
+
+    @Transactional
+    @Override
+    public void updateUserOne(String userId, String password, String userName) {
+
+    }
+
+    @Override
+    public void deleteUserOne(String userId) {
+        userRepository.deleteById(userId);
+    }
+
+    @Override
+    public MUser getLoginUser(String userId) {
+        Optional<MUser> option = userRepository.findById(userId);
+        MUser mUser = option.orElse(null);
+        return mUser;
+    }
+}
