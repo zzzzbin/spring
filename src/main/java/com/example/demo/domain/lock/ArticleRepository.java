ee5c0208935f0cbf7cd452738419aecc563940fe
nguyen.trung <nguyen.trung@sharing-innovations.com>
2023-06-18 16:55:33 +0700

    PessimisticLock

--- src/main/java/com/example/demo/domain/lock/ArticleRepository.java
+++ src/main/java/com/example/demo/domain/lock/ArticleRepository.java
@@ -1,13 +1,19 @@
 package com.example.demo.domain.lock;
 
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
+import javax.persistence.LockModeType;
 
 public interface ArticleRepository extends CrudRepository<Article, Long> {
     //In particular, the query condition must be an index column. If it is not an index,
     // it will become a table lock and lock the entire table.
     @Query(value = "select * from article a where a.id = :id for update", nativeQuery = true)
     Optional<Article> findArticleForUpdate(Long id);
+
+    @Lock(value = LockModeType.PESSIMISTIC_WRITE)
+    @Query("select a from Article a where a.id = :id")
+    Optional<Article> findArticleWithPessimisticLock(Long id);
 }
--- src/main/java/com/example/demo/domain/lock/CommentService.java
+++ src/main/java/com/example/demo/domain/lock/CommentService.java
@@ -2,6 +2,7 @@ package com.example.demo.domain.lock;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
@@ -15,9 +16,11 @@ public class CommentService {
     @Autowired
     private CommentRepository commentRepository;
 
+    @Transactional //You can only lock something in the context of a database transaction.
     public void postComment(Long articleId, String content) {
 //        Optional<Article> articleOptional = articleRepository.findById(articleId);
-        Optional<Article> articleOptional = articleRepository.findArticleForUpdate(articleId);
+//        Optional<Article> articleOptional = articleRepository.findArticleForUpdate(articleId);
+        Optional<Article> articleOptional = articleRepository.findArticleWithPessimisticLock(articleId);
 
         if (!articleOptional.isPresent()) {
             throw new RuntimeException("no corresponding article");
